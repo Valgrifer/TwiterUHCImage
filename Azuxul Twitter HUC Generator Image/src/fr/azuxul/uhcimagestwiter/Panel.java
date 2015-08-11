de462116ed0716eefad1acabb3d5a7e9510f45c6
BuildTools <unconfigured@null.spigotmc.org>
2015-08-11 23:44:45 +0200

    update my logo XP

--- Azuxul Twitter HUC Generator Image/src/fr/azuxul/uhcimagestwiter/Panel.java
+++ Azuxul Twitter HUC Generator Image/src/fr/azuxul/uhcimagestwiter/Panel.java
@@ -20,7 +20,8 @@ public class Panel extends JPanel {
     public void paintComponent(Graphics g){
 
         Image img = null;
-        Image logo = null;
+        Image alogo = null;
+        Image vlogo = null;
         String name = Main.getWindow().textName.getText().length() <= 0?"NAME":Main.getWindow().textName.getText();
         String team = Main.getWindow().textTeam.getText().length() <=0?"FFA":Main.getWindow().textTeam.getText().equalsIgnoreCase("RVB")?"RvB":"FFA";
         String date = Main.getWindow().textDate.getText().length() <= 0?"DATE":Main.getWindow().textDate.getText();
@@ -44,7 +45,8 @@ public class Panel extends JPanel {
         	else
         	{try{img = ImageIO.read(new File(b.getPath()));}catch (IOException e) {}}
         	
-            logo = ImageIO.read(Main.class.getResource("/assets/textures/azuxulLogo.png"));
+            alogo = ImageIO.read(Main.class.getResource("/assets/textures/azuxulLogo.png"));
+            vlogo = ImageIO.read(Main.class.getResource("/assets/textures/valgriferLogo.png"));
         } catch (IOException e) {
             e.printStackTrace();
         };
@@ -84,7 +86,7 @@ public class Panel extends JPanel {
 
         g2D.setFont(new Font("Sans", 0, 10));
         g2D.setColor(Color.WHITE);
-        g2D.drawString("Azuxul", 390, y-15);
+        g2D.drawString("Azuxul & Valgrifer", 340, y-15);
         g2D.setFont(new Font(g2D.getFont().getName(), g2D.getFont().getStyle(), 18));
         g2D.drawString(name + " " + team + " - " + date, 25, 35);
         g2D.drawString("- Scénarios: " +  sc1 , 25, 80); //Max char 34
@@ -100,7 +102,8 @@ public class Panel extends JPanel {
 
         int imgX = (this.getWidth() - bufferedImage.getWidth())/32, imgY = (this.getHeight() - bufferedImage.getHeight())/2;
         g.drawImage(bufferedImage, imgX, imgY, this);
-        g.drawImage(logo, 855, imgY, 155, 155, this);
+        g.drawImage(alogo, 855, 75, 155, 155, this);
+        g.drawImage(vlogo, 875, 0, 110, 110, this);
 
         if(Main.save){
 
